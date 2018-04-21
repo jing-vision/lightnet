abc8bddaa918fd0b49ee7c88426dd206fd22fb60
vinjn <vinjn.z@gmail.com>
2018-04-21 22:18:16 +0800

    Update yolo-template

--- networks/yolo-template/train.cmd
+++ networks/yolo-template/train.cmd
@@ -1,1 +1,1 @@
-..\..\darknet\build\darknet\x64\darknet.exe detector train obj.data yolo-obj.cfg
+..\..\darknet\build\darknet\x64\darknet.exe detector train obj.data yolo-obj.cfg ..\..\assets\darknet19_448.conv.23
