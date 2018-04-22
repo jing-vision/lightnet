30f5b2ca82fde580b4ca63aa59dcb5592cb0b6b5
vinjn <vinjn.z@gmail.com>
2018-04-22 22:29:17 +0800

    Improve build.bat

---
+++ build.bat
@@ -0,0 +1,12 @@
+REM Build
+msbuild darknet\build\darknet\darknet.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
+msbuild Yolo_mark\yolo_mark.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
+msbuild yolo2_light\yolo_gpu.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
+robocopy C:\opencv_3.0\opencv\build\x64\vc14\bin\ %~dp0\darknet\build\darknet\x64\ opencv_world340.dll
+robocopy C:\opencv_3.0\opencv\build\x64\vc14\bin\ %~dp0\Yolo_mark\x64\Release\ opencv_world340.dll
+
+REM Deploy to bin/
+robocopy darknet\build\darknet\x64\ bin\ darknet.exe
+robocopy Yolo_mark\x64\Release\ bin\ yolo_mark.exe
+robocopy yolo2_light\bin\ bin\ yolo_gpu.exe
+robocopy C:\opencv_3.0\opencv\build\x64\vc14\bin\ bin\ opencv_world340.dll
