72c621532cc0890cbe81a666d7a4267029fb03e5
vinjn <vinjn.z@gmail.com>
2018-05-01 10:38:26 +0800

    Add bin/darknet.py

--- build.bat
+++ build.bat
@@ -1,16 +1,14 @@
 REM Build
 msbuild vs2015\darknet.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
 msbuild vs2015\darknet.sln /p:Configuration=Release-CPU /p:Platform=x64 /v:minimal
-REM msbuild darknet\build\darknet\yolo_cpp_dll.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
+msbuild darknet\build\darknet\yolo_cpp_dll.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
 msbuild Yolo_mark\yolo_mark.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
 msbuild yolo2_light\yolo_gpu.sln /p:Configuration=Release /p:Platform=x64 /v:minimal
-robocopy C:\opencv_3.0\opencv\build\x64\vc14\bin\ %~dp0\darknet\build\darknet\x64\ opencv_world340.dll
-robocopy C:\opencv_3.0\opencv\build\x64\vc14\bin\ %~dp0\Yolo_mark\x64\Release\ opencv_world340.dll
 
 REM Deploy to bin/
 REM robocopy darknet\build\darknet\x64\ bin\ darknet.exe
 REM robocopy darknet\build\darknet\x64\ bin\ darknet_no_gpu.exe
-REM robocopy darknet\build\darknet\x64\ bin\ yolo_cpp_dll.dll
+robocopy darknet\build\darknet\x64\ bin\ yolo_cpp_dll.dll
 robocopy darknet\build\darknet\x64\ bin\ pthreadVC2.dll
 robocopy darknet\cfg bin\cfg /E
 robocopy darknet\data bin\data /E
