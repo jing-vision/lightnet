cbea941ed36a26016a27133388d3209b679259fc
vinjn <vinjn.z@gmail.com>
2018-05-01 11:01:53 +0800

    yolo9000

--- networks/yolo9000/inference.bat
+++ networks/yolo9000/inference.bat
@@ -1,4 +1,1 @@
-set CWD=%~dp0
-cd ..\..\bin
-call darknet.exe detector demo cfg\combine9k.data cfg\yolo9000.cfg %CWD%\yolo9000.weights
-cd %CWD%
+..\..\bin\darknet.exe detector demo cfg\combine9k.data cfg\yolo9000.cfg yolo9000.weights
