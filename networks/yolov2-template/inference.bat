3b22e0e812cc8831a0bba93321c8422dc4e108e2
vinjn <vinjn.z@gmail.com>
2018-05-07 22:33:44 +0800

    template - update inference.bat

--- networks/yolov2-template/inference.bat
+++ networks/yolov2-template/inference.bat
@@ -1,1 +1,1 @@
-..\..\bin\darknet.exe detector demo obj.data yolo-obj.cfg weights\yolo-obj_100.weights %*
+..\..\bin\darknet.exe detector demo obj.data yolo-obj.cfg weights\yolo-obj_100.weights  -c 0  %* -thresh 0.2
