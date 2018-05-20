5a5b259d251ad162f3c25459e2b5407fad05a0e0
vinjn <vinjn.z@gmail.com>
2018-05-20 11:20:15 +0800

    gitignore

--- build.bat
+++ build.bat
@@ -1,16 +1,22 @@
 REM Build
-msbuild vs2015\darknet.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
-msbuild vs2015\darknet.sln /p:Configuration=Release-CPU /p:Platform=x64 /v:minimal /m
-msbuild darknet\build\darknet\yolo_cpp_dll.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
+
+msbuild proj_exe\darknet.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
+msbuild proj_exe\darknet.sln /p:Configuration=Release-CPU /p:Platform=x64 /v:minimal /m
+
+msbuild proj_dll\yolo_cpp_dll.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
+msbuild proj_dll\yolo_cpp_dll.sln /p:Configuration=Release-CPU /p:Platform=x64 /v:minimal /m
+
 msbuild Yolo_mark\yolo_mark.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
 msbuild yolo2_light\yolo_gpu.sln /p:Configuration=Release /p:Platform=x64 /v:minimal /m
 
 REM Deploy to bin/
+
 REM robocopy darknet\build\darknet\x64\ bin\ darknet.exe
 REM robocopy darknet\build\darknet\x64\ bin\ darknet_no_gpu.exe
-robocopy darknet\build\darknet\x64\ bin\ yolo_cpp_dll.dll
+REM robocopy darknet\build\darknet\x64\ bin\ yolo_cpp_dll.dll
+
 robocopy darknet\build\darknet\x64\ bin\ pthreadVC2.dll
-robocopy darknet\darknet.py bin darknet.py
+robocopy darknet\ bin\ darknet.py
 robocopy darknet\cfg bin\cfg /E
 robocopy darknet\data bin\data /E
 robocopy Yolo_mark\x64\Release\ bin\ yolo_mark.exe
