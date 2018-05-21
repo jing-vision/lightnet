ab8e1358379e8ed9c1b7727669b7c0e89920e135
vinjn <vinjn.z@gmail.com>
2018-05-21 21:51:20 +0800

    train.bat - add helper instruction

--- networks/yolov2-template/train.bat
+++ networks/yolov2-template/train.bat
@@ -1,1 +1,2 @@
 ..\..\bin\darknet.exe detector train obj.data yolo-obj.cfg ..\..\bin\darknet19_448.conv.23
+REM ..\..\bin\darknet.exe detector train obj.data yolo-obj.cfg weights\yolo-obj_200.weights
