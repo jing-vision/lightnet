1a2a38aeaf159577dedd74d8f9e848e2aa908580
vinjn <vinjn.z@gmail.com>
2018-05-27 00:20:23 +0800

    auto_labeller.py now writes "obj.cfg"

--- bin/auto_labeller.py
+++ bin/auto_labeller.py
@@ -15,6 +15,8 @@ if PY3:
 
 import numpy as np
 import cv2 as cv
+import configparser
+from collections import OrderedDict
 
 # file names config
 category_folders = glob.glob('img/*')
@@ -23,6 +25,7 @@ g_image_filenames = []
 train_txt = 'train.txt'
 obj_data = 'obj.data'
 obj_names = 'obj.names'
+obj_cfg = 'obj.cfg'
 
 def update_category(category_id):
     global g_image_filenames, g_category_id
@@ -34,13 +37,16 @@ def main():
 
     cwd = os.getcwd()
 
+    num_classes = len(category_folders)
+
     with open(obj_data, 'w') as obj_data_fp:
-        obj_data_fp.write('classes=%d\n' % len(category_folders))
-        obj_data_fp.write('train  =%s/train.txt\n' % (cwd))
-        obj_data_fp.write('valid  =%s/train.txt\n' % (cwd))
-        obj_data_fp.write('labels =%s/obj.names\n' % (cwd))
-        obj_data_fp.write('names =%s/obj.names\n' % (cwd))
-        obj_data_fp.write('backup =%s/weights/\n' % (cwd))
+        obj_data_fp.write('classes=%d\n' % num_classes)
+        obj_data_fp.write('train=%s/train.txt\n' % (cwd))
+        obj_data_fp.write('valid=%s/train.txt\n' % (cwd))
+        obj_data_fp.write('labels=%s/obj.names\n' % (cwd))
+        obj_data_fp.write('names=%s/obj.names\n' % (cwd))
+        obj_data_fp.write('backup=%s/weights/\n' % (cwd))
+        obj_data_fp.write('top=5\n')
 
     train_txt_fp = open(train_txt, 'w')
     obj_names_fp = open(obj_names, 'w')
@@ -53,6 +59,33 @@ def main():
         for image_filename in image_filenames:
             train_txt_fp.write(os.path.abspath(image_filename))
             train_txt_fp.write('\n')
+
+    class multidict(OrderedDict):
+        _unique = 0   # class variable
+
+        def __setitem__(self, key, val):
+            if isinstance(val, dict):
+                self._unique += 1
+                val['original_key'] = key
+                key += str(self._unique)
+            OrderedDict.__setitem__(self, key, val)
+    config = configparser.ConfigParser(
+        defaults=None, dict_type=multidict, strict=False)
+    config.read(obj_cfg)
+    print(config.sections())
+    config._sections['net1']['batch'] = 32
+    config._sections['net1']['subdivisions'] = 4
+    config._sections['net1']['max_batches'] = 10000
+    config._sections['convolutional25']['filters'] = num_classes
+
+    with open(obj_cfg, 'w') as configfile:
+        for key in config._sections:
+            section = config._sections[key]
+            configfile.write('[%s]\n' % section['original_key'])
+            del section['original_key']
+            for field in section:
+                configfile.write('%s=%s\n' % (field, section[field]))
+            configfile.write('\n')
 
 if __name__ == '__main__':
     main()
