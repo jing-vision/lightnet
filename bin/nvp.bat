10c717d3530f9df0b90e735471d89492e67384c7
vinjn <vinjn.z@gmail.com>
2018-06-11 10:34:41 +0800

    nvp.bat - improve TIMESTAMP

--- bin/nvp.bat
+++ bin/nvp.bat
@@ -1,2 +1,4 @@
-set TIMESTAMP=%DATE:/=-%-%TIME::=-%
+set mydate=%date:/=%
+set mytime=%time::=%
+set TIMESTAMP=%mydate: =_%_%mytime:.=_%
 call nvprof --print-gpu-trace --demangling off --csv --log-file nvprof-%1-%TIMESTAMP%.csv %*
