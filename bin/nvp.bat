49af3c58c9233ecc129931d96804947e7b2f0dfb
vinjn <vinjn.z@gmail.com>
2018-06-11 10:48:12 +0800

    nvp.bat - add --print-api-trace

--- bin/nvp.bat
+++ bin/nvp.bat
@@ -1,4 +1,4 @@
 set mydate=%date:/=%
 set mytime=%time::=%
 set TIMESTAMP=%mydate: =_%_%mytime:.=_%
-call nvprof --print-gpu-trace --demangling off --csv --log-file nvprof-%1-%TIMESTAMP%.csv %*
+call nvprof --print-api-trace --print-gpu-trace --demangling off --csv --log-file nvprof-%1-%TIMESTAMP%.csv %*
