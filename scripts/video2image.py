f9d0a80672031c1c3075c171c4ece7a99ff80c11
vinjn <vinjn.z@gmail.com>
2018-08-08 14:39:13 +0800

    video2image.py - add interactive mode

--- scripts/video2image.py
+++ scripts/video2image.py
@@ -3,9 +3,7 @@
 '''
 1. 安装 Python 3.6 https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe
 2. ctrl + r -> cmd 进命令行，输入 pip install opencv-python
-3. 运行目录下的 run.bat
-
-run.bat 中的内容解释
+3. 运行范例代码，从默认摄像头读取画面，存到 img 文件夹内
 python video2image.py --source 0 --w 1024 --h 768 --shotdir img
 
 --source 0 摄像头设备的编号，从零开始
@@ -41,28 +39,40 @@ if __name__ == '__main__':
         description='Dump image from camera device')
     parser.add_argument(
         '--source', type=int, default=0, help='Path to image or video. Skip to capture frames from camera')
-    parser.add_argument('--w', type=int, default=1024)
-    parser.add_argument('--h', type=int, default=768)
-    parser.add_argument('--shotdir', default='img')
-    parser.add_argument('--prefix', default='shot')
+    parser.add_argument('--w', type=int, default=640)
+    parser.add_argument('--h', type=int, default=480)
+    parser.add_argument('--shotdir')
     args = parser.parse_args()
 
-    if not os.path.exists(args.shotdir):
-        os.mkdir(args.shotdir)
+    interactive_mode = args.shotdir is None
+    needs_new_name = True
+
+    if interactive_mode:
+        print("Press SPACE in Capture window to input new folder name")
 
     cap = create_capture(args)
     prev_datetime = datetime.datetime.now()
     while True:
         ret, img = cap.read()
-        cv.imshow('capture', img)
+        cv.imshow('Capture', img)
         ch = cv.waitKey(1)
+        if ch == 32:
+            # SPACE
+            needs_new_name = True
         if ch == 27:
+            # ESC
             break
         now = datetime.datetime.now()
         if (now - prev_datetime).microseconds > 200000:
+            if interactive_mode and needs_new_name:
+                args.shotdir = input("Input folder name: ")
+                needs_new_name = False
+            if not os.path.exists(args.shotdir):
+                os.mkdir(args.shotdir)
+
             now_string = now.strftime("%Y-%h-%d-%H-%M-%S-%f")
             prev_datetime = now
-            fn = '%s/cam%d_%s_%s.jpg' % (args.shotdir, args.source, args.prefix, now_string)
+            fn = '%s/cam%d_%s.jpg' % (args.shotdir, args.source, now_string)
             cv.imwrite(fn, img)
             print(fn, 'saved')
     cv.destroyAllWindows()
