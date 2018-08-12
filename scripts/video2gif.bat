f45fe97690b0663c2fb6079d46b26fa493c768c4
vinjn <vinjn.z@gmail.com>
2018-08-12 10:01:48 +0800

    Ease the usage of video2gif.bat

--- scripts/video2gif.bat
+++ scripts/video2gif.bat
@@ -1,5 +1,7 @@
+REM video2gif /path/to/video/file
+
 set palette=palette.png
 set filters=fps=15,scale=320:-1:flags=lanczos
 
 ffmpeg -v warning -i %1 -vf %filters%,palettegen -y %palette%
-ffmpeg -v warning -i %1 -i %palette% -lavfi %filters%" [x]; [x][1:v] paletteuse" -y %2
+ffmpeg -v warning -i %1 -i %palette% -lavfi %filters%" [x]; [x][1:v] paletteuse" -y %1.gif
