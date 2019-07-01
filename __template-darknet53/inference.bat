b28522af9cfc6147f52dc78c234eb702bbede39e
vinjn <vinjn.z@gmail.com>
2019-07-01 11:14:20 +0800

    Expose "REM set CUDA_VISIBLE_DEVICES=-1"

--- __template-darknet53/inference.bat
+++ __template-darknet53/inference.bat
@@ -1,1 +1,3 @@
+REM set CUDA_VISIBLE_DEVICES=-1
+set DARKNET_FORCE_CPU=
 python ..\scripts\classifier.py --camera=0 --weights=weights\obj_last.weights
--- __template-yolov2/inference.bat
+++ __template-yolov2/inference.bat
@@ -1,1 +1,2 @@
+REM set CUDA_VISIBLE_DEVICES=-1
 python ..\scripts\detector.py --camera=0 --weights=weights\obj_last.weights
