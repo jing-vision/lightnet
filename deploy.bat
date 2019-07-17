51ea6e919bb85c9bf147059bac592087a33d2a16
vinjn <vinjn.z@gmail.com>
2019-07-17 20:57:43 +0800

    deploy.bat - add __template-darknet19

--- deploy.bat
+++ deploy.bat
@@ -11,7 +11,8 @@ robocopy bin %OUTPUT%/bin pthreadVC2.dll
 robocopy bin %OUTPUT%/bin yolo_cpp_dll.dll
 robocopy bin %OUTPUT%/bin feature-viz.exe
 robocopy bin %OUTPUT%/bin darknet.exe
-robocopy bin %OUTPUT%/bin darknet.conv.13
+REM robocopy bin %OUTPUT%/bin darknet.conv.13
+REM robocopy bin %OUTPUT%/bin darknet19.conv.23
 robocopy bin %OUTPUT%/bin darknet19_448.conv.23
 robocopy bin/cfg %OUTPUT%/bin/cfg
 robocopy bin/data %OUTPUT%/bin/data
@@ -22,4 +23,5 @@ robocopy scripts %OUTPUT%/scripts yolo_cpp_dll.dll
 robocopy scripts/bat %OUTPUT%/scripts/bat
 
 robocopy __template-darknet %OUTPUT%/__template-darknet /MIR
+robocopy __template-darknet19 %OUTPUT%/__template-darknet19 /MIR
 robocopy __template-darknet19_448 %OUTPUT%/__template-darknet19_448 /MIR
