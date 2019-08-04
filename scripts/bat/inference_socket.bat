f35e1e29c7ae93d4dfa459cf512e06f8bb7d5d3c
vinjn <vinjn.z@gmail.com>
2019-08-04 10:40:42 +0800

    inference_socket.bat - add threshold

--- scripts/bat/inference_socket.bat
+++ scripts/bat/inference_socket.bat
@@ -1,3 +1,3 @@
 REM set CUDA_VISIBLE_DEVICES=-1
 set PATH=%PATH%;%~dp0\..\bin
-python ..\scripts\classifier.py --socket=5000 --weights=weights\obj_last.weights
+python ..\scripts\classifier.py --socket=5000 --weights=weights\obj_last.weights --display_confidence=0.1
