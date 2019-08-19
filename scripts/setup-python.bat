d9239ac048c0274255fed1d08a9d1aeedccc007a
vincentz <vincentz@nvidia.com>
2019-08-19 13:31:16 +0800

    Use pip mirror

--- scripts/setup-python.bat
+++ scripts/setup-python.bat
@@ -1,4 +1,4 @@
 REM install https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-pip install flask
-pip install opencv-python
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple flask
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple opencv-python
 pause
