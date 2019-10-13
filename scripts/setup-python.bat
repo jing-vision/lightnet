e5dad8d9531371e7e4ee561f094c482a2054bb82
vinjn <vinjn.z@gmail.com>
2019-10-13 16:16:59 +0800

    Add "--upgrade" to setup-python.bat

--- scripts/setup-python.bat
+++ scripts/setup-python.bat
@@ -1,4 +1,4 @@
 REM install https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple flask
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple opencv-python
+pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple flask
+pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple opencv-python
 pause
