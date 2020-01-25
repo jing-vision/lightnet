d8aa609bdc975a3db820e74c022903419871471c
vinjn <vinjn.z@gmail.com>
2020-01-25 23:17:53 +0800

    config._sections['net1']['batch'] = 64     config._sections['net1']['max_batches'] = 5000

--- scripts/auto_labeller.py
+++ scripts/auto_labeller.py
@@ -5,6 +5,10 @@
 
 # Python 2/3 compatibility
 from __future__ import print_function
+from collections import OrderedDict
+import configparser
+import cv2 as cv
+import numpy as np
 import glob
 import os
 import sys
@@ -14,10 +18,6 @@ PY3 = sys.version_info[0] == 3
 if PY3:
     xrange = range
 
-import numpy as np
-import cv2 as cv
-import configparser
-from collections import OrderedDict
 
 # file names config
 category_folders = glob.glob('img/*')
@@ -27,10 +27,11 @@ obj_data = 'obj.data'
 obj_names = 'obj.names'
 obj_cfg = 'obj.cfg'
 
+
 def main():
     print(__doc__)
 
-    cwd = '.' #os.getcwd()
+    cwd = '.'  # os.getcwd()
 
     num_classes = len(category_folders)
 
@@ -52,10 +53,11 @@ def main():
         obj_names_fp.write(category)
         obj_names_fp.write('\n')
         image_filenames = glob.glob(category + '/**/*.jpg', recursive=True)
-        image_filenames.extend(glob.glob(category + '/**/*.png', recursive=True))
+        image_filenames.extend(
+            glob.glob(category + '/**/*.png', recursive=True))
 
         for image_filename in image_filenames:
-            if random.random() < 0.1: # 10% for validation
+            if random.random() < 0.3:  # 10% for validation
                 valid_txt_fp.write(os.path.abspath(image_filename))
                 valid_txt_fp.write('\n')
             else:
@@ -76,9 +78,9 @@ def main():
         defaults=None, dict_type=multidict, strict=False)
     config.read(obj_cfg)
     print(config.sections())
-    config._sections['net1']['batch'] = 32
+    config._sections['net1']['batch'] = 64
     config._sections['net1']['subdivisions'] = 4
-    config._sections['net1']['max_batches'] = 10000
+    config._sections['net1']['max_batches'] = 5000
     config._sections['net1']['learning_rate'] = 0.001
 
     conv_reverse_id = 0
@@ -102,6 +104,7 @@ def main():
             for field in section:
                 configfile.write('%s=%s\n' % (field, section[field]))
             configfile.write('\n')
+
 
 if __name__ == '__main__':
     main()
